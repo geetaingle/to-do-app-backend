d8f23185ee21954c03173d413931e0275039887b
Geeta Ingle <geeta.i@softlinklocal.com>
2023-01-20 13:45:22 +0530

    connected to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/message", (req, res) => {
-  res.json({ message: "To-Do App" });
+// mongoose.set("useFindandModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlPArser: true }, () => {
+  console.log("connected to db");
+
+  app.listen(8000, () => {
+    console.log("server running on port 8000");
+  });
 });
 
-app.listen(8000, () => {
-  console.log("server running on port 8000");
+app.get("/message", (req, res) => {
+  res.json({ message: "To-Do App" });
 });
